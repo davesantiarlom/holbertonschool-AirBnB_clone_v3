48c1a1e96bc2bf0b6cc8caea62c43f6483b514c0
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-12-15 07:21:15 -0500

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
-"""
-creating blueprint for flask
-"""
-from flask import Flask, Blueprint,  make_response, jsonify
+"""app.py"""
 from models import storage
-import os
-from flask_cors import CORS
 from api.v1.views import app_views
-
-
+from werkzeug.exceptions import HTTPException
+from flask import Flask
+from os import getenv
+from flask import jsonify
+from flask_cors import CORS
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, resources={"/*": {"origins": '0.0.0.0'}})
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {'origins': ['0.0.0.0']}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """ closes down current session """
+def teardown_appcontext(error):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    """
-    a handler for 404 errors that returns a
-    JSON-formatted 404 status code response
-    """
-    return (jsonify({"error": "Not found"}), 404)
+def page_404(error):
+    """ Return a custom 404 error """
+    err_dict = {"error": "Not found"}
+    return jsonify(err_dict), 404
 
 
 if __name__ == "__main__":
-    hosts = os.getenv("HBNB_API_HOST", default='0.0.0.0')
-    ports = int(os.getenv("HBNB_API_PORT", default=5000))
-    app.run(host=hosts, port=ports, threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
