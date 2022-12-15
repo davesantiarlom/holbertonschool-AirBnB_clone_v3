f595744d119dbafc1954e0d49c8025ef8d184f6d
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-12-15 08:20:09 -0500

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-"""app.py"""
-from models import storage
-from api.v1.views import app_views
-from werkzeug.exceptions import HTTPException
-from flask import Flask
+"""
+Sets up Flask application
+"""
+
+
 from os import getenv
-from flask import jsonify
+from flask import Flask, make_response, jsonify
 from flask_cors import CORS
+
+from api.v1.views import app_views
+from models import storage
+
 app = Flask(__name__)
-CORS(app, resources={"/*": {"origins": '0.0.0.0'}})
+CORS(app, orgins='0.0.0.0')
 app.register_blueprint(app_views)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
-
-@app.teardown_appcontext
-def teardown_appcontext(error):
-    """teardown_appcontext"""
-    storage.close()
 
 
 @app.errorhandler(404)
-def page_404(error):
-    """ Return a custom 404 error """
-    err_dict = {"error": "Not found"}
-    return jsonify(err_dict), 404
+def page_not_found(error):
+    """Returns JSON error repsponse"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True)
+@app.teardown_appcontext
+def teardown(self):
+    """Closes storage session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    api_port = getenv('HBNB_API_PORT', default=5000)
+    app.run(host=api_host, port=int(api_port), threaded=True)
